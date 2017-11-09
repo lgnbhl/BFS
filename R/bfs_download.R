003659488bedb354f5ed9aca1e9248c5fa24e521
Félix Luginbühl <felixl@protonmail.com>
2017-11-09 10:38:26 +0100

    Add files via upload

---
+++ R/bfs_download.R
@@ -0,0 +1,20 @@
+#' Download BFS data
+#'
+#' @export
+
+bfs_download <- function(row, name = "bfsData") {
+  bfsDataPath <- system.file("extdata/", package = "bfsdata")
+  assign("bfsDataPath", bfsDataPath, envir = .GlobalEnv)
+  if(!file.exists(system.file(paste0("extdata/", bfs_metadataSubset[row, 4], ".px"), package = "bfsdata"))) {
+    download.file(url = paste0("https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file=", bfs_metadataSubset[row,4], ""),
+                  destfile = paste0("", bfsDataPath, "/", bfs_metadataSubset[row, 4],".px"))
+  } else {
+    message("WARNING: Dataset already downloaded")
+  }
+  library(pxR) # read px format
+  bfsData <- pxR::read.px(paste0("", bfsDataPath, "/",bfs_metadataSubset[row, 4],".px"))
+  bfsData <- as.data.frame(bfsData)
+  write.csv(bfsData, file = paste0("", bfsDataPath, "/", name,".csv"), row.names = FALSE)
+  assign(paste0("", name,""), bfsData, envir = .GlobalEnv)
+  detach("package:pxR", unload = TRUE) # pxR::as.data.frame in conflict with raster::as.data.frame
+}
