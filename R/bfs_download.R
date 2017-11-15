bcc91015adae9b9d5a94679cb06dfc93aa2a4b4a
Félix Luginbühl <felix.luginbuhl@protonmail.ch>
2017-11-15 18:07:40 +0100

    Update bfs_download.R

--- R/bfs_download.R
+++ R/bfs_download.R
@@ -5,14 +5,14 @@
 bfs_download <- function(row, name = "bfsData") {
   bfsDataPath <- system.file("extdata/", package = "bfsdata")
   assign("bfsDataPath", bfsDataPath, envir = .GlobalEnv)
-  if(!file.exists(system.file(paste0("extdata/", bfs_metadataSubset[row, 4], ".px"), package = "bfsdata"))) {
-    download.file(url = paste0("https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file=", bfs_metadataSubset[row,4], ""),
-                  destfile = paste0("", bfsDataPath, "/", bfs_metadataSubset[row, 4],".px"))
+  if(!file.exists(system.file(paste0("extdata/", bfsMetadataSubset[row, 4], ".px"), package = "bfsdata"))) {
+    download.file(url = paste0("https://www.pxweb.bfs.admin.ch/DownloadFile.aspx?file=", bfsMetadataSubset[row,4], ""),
+                  destfile = paste0("", bfsDataPath, "/", bfsMetadataSubset[row, 4],".px"))
   } else {
     message("WARNING: Dataset already downloaded")
   }
   library(pxR) # read PX files
-  bfsData <- pxR::read.px(paste0("", bfsDataPath, "/",bfs_metadataSubset[row, 4],".px"))
+  bfsData <- pxR::read.px(paste0("", bfsDataPath, "/", bfsMetadataSubset[row, 4],".px"))
   assign(paste0("", name,"_px"), bfsData, envir = .GlobalEnv)
   bfsData <- as.data.frame(bfsData)
   write.csv(bfsData, file = paste0("", bfsDataPath, "/", name,".csv"), row.names = FALSE)
