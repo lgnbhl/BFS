2f2248d3a007b3d20966d8192263e35bd520a1a5
Félix Luginbühl <felix.luginbuhl@protonmail.ch>
2017-11-15 18:06:07 +0100

    Update bfs_search.R

--- R/bfs_search.R
+++ R/bfs_search.R
@@ -7,54 +7,54 @@ bfs_search <- function(word = NULL, word2 = NULL, langage = "de") {
   assign("bfsDataPath", bfsDataPath, envir = .GlobalEnv)
   library(readxl)
   if(langage == "de") {
-    if(!file.exists(system.file("extdata/bfs_metadata_de.xls", package = "bfsdata"))) {
+    if(!file.exists(system.file("extdata/bfsMetadata_de.xls", package = "bfsdata"))) {
     download.file(url = "https://www.pxweb.bfs.admin.ch/ShowCubeList.aspx?px_language=de",
-                  destfile = paste0("", bfsDataPath, "/bfs_metadata_de.xls"))
+                  destfile = paste0("", bfsDataPath, "/bfsMetadata_de.xls"))
     }
-    bfs_metadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfs_metadata_de.xls"))
-    assign("bfs_metadata", bfs_metadata, envir = .GlobalEnv)
+    bfsMetadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfsMetadata_de.xls"))
+    assign("bfsMetadata", bfsMetadata, envir = .GlobalEnv)
   } else if(langage == "fr") {
-    if(!file.exists(system.file("extdata/bfs_metadata_fr.xls", package = "bfsdata"))) {
+    if(!file.exists(system.file("extdata/bfsMetadata_fr.xls", package = "bfsdata"))) {
       download.file(url = "https://www.pxweb.bfs.admin.ch/ShowCubeList.aspx?px_language=fr",
-                    destfile = paste0("", bfsDataPath, "/bfs_metadata_fr.xls"))
+                    destfile = paste0("", bfsDataPath, "/bfsMetadata_fr.xls"))
     }
-    bfs_metadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfs_metadata_fr.xls"))
-    assign("bfs_metadata", bfs_metadata, envir = .GlobalEnv)
+    bfsMetadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfsMetadata_fr.xls"))
+    assign("bfsMetadata", bfsMetadata, envir = .GlobalEnv)
   } else if (langage == "en") {
-    if(!file.exists(system.file("extdata/bfs_metadata_en.xls", package = "bfsdata"))) {
+    if(!file.exists(system.file("extdata/bfsMetadata_en.xls", package = "bfsdata"))) {
       download.file(url = "https://www.pxweb.bfs.admin.ch/ShowCubeList.aspx?px_language=en",
-                    destfile = paste0("", bfsDataPath, "/bfs_metadata_en.xls"))
+                    destfile = paste0("", bfsDataPath, "/bfsMetadata_en.xls"))
     }
-    bfs_metadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfs_metadata_en.xls"))
-    assign("bfs_metadata", bfs_metadata, envir = .GlobalEnv)
+    bfsMetadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfsMetadata_en.xls"))
+    assign("bfsMetadata", bfsMetadata, envir = .GlobalEnv)
   } else if (langage == "it") {
-    if(!file.exists(system.file("extdata/bfs_metadata_it.xls", package = "bfsdata"))) {
+    if(!file.exists(system.file("extdata/bfsMetadata_it.xls", package = "bfsdata"))) {
       download.file(url = "https://www.pxweb.bfs.admin.ch/ShowCubeList.aspx?px_language=it",
-                    destfile = paste0("", bfsDataPath, "/bfs_metadata_it.xls"))
+                    destfile = paste0("", bfsDataPath, "/bfsMetadata_it.xls"))
     }
-    bfs_metadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfs_metadata_it.xls"))
-    assign("bfs_metadata", bfs_metadata, envir = .GlobalEnv)
+    bfsMetadata <- readxl::read_excel(paste0("", bfsDataPath, "/bfsMetadata_it.xls"))
+    assign("bfsMetadata", bfsMetadata, envir = .GlobalEnv)
   } else {
     message("WARNING: choose between German, French, English or Italien (langage = “de“, “fr“, “en“, “it“)")
   }
-  if(exists("bfs_metadata")) {
-    bfs_metadata <- na.omit(bfs_metadata)
-    colnames(bfs_metadata)[1] <- "Title"
-    colnames(bfs_metadata)[2] <- "Timespan"
-    colnames(bfs_metadata)[3] <- "Last Update"
-    colnames(bfs_metadata)[4] <- "Link"
-    colnames(bfs_metadata)[5] <- "Languages available"
-    # bfs_metadata$ID <- seq.int(nrow(bfs_metadata)) # add an ID variable
-    assign("bfs_metadata", bfs_metadata, envir = .GlobalEnv)
+  if(exists("bfsMetadata")) {
+    bfsMetadata <- na.omit(bfsMetadata)
+    colnames(bfsMetadata)[1] <- "Title"
+    colnames(bfsMetadata)[2] <- "Timespan"
+    colnames(bfsMetadata)[3] <- "Last Update"
+    colnames(bfsMetadata)[4] <- "Link"
+    colnames(bfsMetadata)[5] <- "Languages available"
+    # bfsMetadata$ID <- seq.int(nrow(bfsMetadata)) # add an ID variable
+    assign("bfsMetadata", bfsMetadata, envir = .GlobalEnv)
     # URL: https://stackoverflow.com/questions/13187414/r-grep-is-there-an-and-operator
     if(missing(word2)) {
-      bfs_metadataSubset <- bfs_metadata[grep(paste0("", word, ""), bfs_metadata$Title, ignore.case = TRUE), ]
-      assign("bfs_metadataSubset", bfs_metadataSubset, envir = .GlobalEnv)
-      print.table(bfs_metadataSubset$Title)
+      bfsMetadataSubset <- bfsMetadata[grep(paste0("", word, ""), bfsMetadata$Title, ignore.case = TRUE), ]
+      assign("bfsMetadataSubset", bfsMetadataSubset, envir = .GlobalEnv)
+      print.table(bfsMetadataSubset$Title)
     } else {
-      bfs_metadataSubset <- bfs_metadata[grep(paste0("(?=.*", word,")(?=.*", word2, ")"), bfs_metadata$Title, perl = TRUE, ignore.case = TRUE), ]
-      assign("bfs_metadataSubset", bfs_metadataSubset, envir = .GlobalEnv)
-      print.table(bfs_metadataSubset$Title)
+      bfsMetadataSubset <- bfsMetadata[grep(paste0("(?=.*", word,")(?=.*", word2, ")"), bfsMetadata$Title, perl = TRUE, ignore.case = TRUE), ]
+      assign("bfsMetadataSubset", bfsMetadataSubset, envir = .GlobalEnv)
+      print.table(bfsMetadataSubset$Title)
     }
   }
 }
