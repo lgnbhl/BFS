5ed38ca2a28f0e2a09b6a08c0fcd6e48821f37b4
Félix Luginbühl <felix.luginbuhl@protonmail.ch>
2017-11-19 14:53:24 +0100

    Update bfs_download.R

--- R/bfs_download.R
+++ R/bfs_download.R
@@ -11,11 +11,9 @@ bfs_download <- function(row, name = "bfsData") {
   } else {
     message("WARNING: Dataset already downloaded")
   }
-  library(pxR) # read PX files
   bfsData <- pxR::read.px(paste0("", bfsDataPath, "/", bfsMetadataSubset[row, 4],".px"))
   assign(paste0("", name,"_px"), bfsData, envir = .GlobalEnv)
   bfsData <- as.data.frame(bfsData)
   write.csv(bfsData, file = paste0("", bfsDataPath, "/", name,".csv"), row.names = FALSE)
   assign(paste0("", name,""), bfsData, envir = .GlobalEnv)
-  detach("package:pxR", unload = TRUE) # pxR::as.data.frame in conflict with raster::as.data.frame
 }
