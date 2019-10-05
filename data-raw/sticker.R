2384cd4fe3953db6254f5056de7bc8e198960e49
lgnbhl <felix.luginbuhl@protonmail.ch>
2019-10-04 22:13:37 -0500

    New sticker

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -3,23 +3,23 @@ library(hexSticker)
 library(magick)
 library(magrittr)
 
-hexSticker::sticker("https://upload.wikimedia.org/wikipedia/commons/b/b5/Wenger_EvoGrip_S17.JPG",
+hexSticker::sticker("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/1000px-Flag_of_Switzerland.svg.png",
                     package="BFS",
                     p_color = "black",
                     p_size = 10,
-                    p_y = 1.45,
+                    p_y = 1,
                     spotlight = FALSE,
                     h_size = 2,
                     h_color = "black",
                     h_fill = "white",
-                    s_x = 1.01, s_y = 0.86, s_width = 0.9,
-                    url="BFS - R package",
-                    u_size = 2,
-                    u_y = 0.15,
-                    u_x = 1.05,
+                    s_x = 1, s_y = 1, s_width = 1,
+                    url="felixluginbuhl.com/BFS",
+                    u_size = 1.6,
                     u_color = "black",
                     filename="man/figures/BFS.png")
 
-bfs <- magick::image_read("man/figures/BFS.png")
+# MOD with Gimp: removing red borders.
+
+bfs <- magick::image_read("man/figures/BFS_mod.png")
 magick::image_scale(bfs, "130") %>%
   magick::image_write(path = "man/figures/logo.png", format = "png")
