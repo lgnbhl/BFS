72b71ccbf554b23594525e0353e5abc4907be05f
lgnbhl <felix.luginbuhl@protonmail.ch>
2023-09-14 09:45:30 +0200

    remove encoding tests

--- R/bfs_get_base_maps.R
+++ R/bfs_get_base_maps.R
@@ -47,8 +47,7 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
   
   #list all files
   files_all <- list.files(path_base_map, recursive = TRUE, full.names = TRUE)
-  #Encoding(files_all) <- "UTF-8" # fix bug for Mac and Linux
-  
+
   if(identical(files_all, character(0))) {
     stop("Error in listing available base map files", call. = FALSE)
   }
@@ -84,5 +83,5 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
   if(identical(file_selected, character(0))) {
     stop("No related file found. Please use other argument values.", call. = FALSE)
   }
-  sf::read_sf(file_selected, options = "ENCODING=latin1")
+  sf::read_sf(file_selected)
 }
