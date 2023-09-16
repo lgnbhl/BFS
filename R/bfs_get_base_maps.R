5f40ce3051a0d03ee946aa5adfca201ca44bd165
lgnbhl <felix.luginbuhl@protonmail.ch>
2023-09-16 20:29:02 +0200

    test ignore.case = TRUE using grep, see https://stackoverflow.com/a/64651758

--- R/bfs_get_base_maps.R
+++ R/bfs_get_base_maps.R
@@ -52,7 +52,7 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
     stop("Error in listing available base map files", call. = FALSE)
   }
   
-  files_format <- grep(pattern = paste0(".", format, "$"), x = files_all, value = TRUE, useBytes = TRUE)
+  files_format <- grep(pattern = paste0(".", format, "$"), x = files_all, value = TRUE, useBytes = TRUE, ignore.case = TRUE)
   # category, i.e. search file with "gf_ch" or "vf_ch"
   if(category == "total_area" || category == "gf") {
     category_selected <- "gf_ch"
@@ -61,14 +61,14 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
   } else {
     category_selected <- category #other options, for example for 'k4seenyyyymmdd11_ch2007Poly'
   }
-  files_cat <- grep(pattern = category_selected, x = files_format, value = TRUE, useBytes = TRUE)
+  files_cat <- grep(pattern = category_selected, x = files_format, value = TRUE, useBytes = TRUE, ignore.case = TRUE)
   # type, i.e. "Poly" or "Pnts"
-  files_poly <- grep(pattern = paste0(type, ".", format, "$"), x = files_cat, value = TRUE, useBytes = TRUE)
+  files_poly <- grep(pattern = paste0(type, ".", format, "$"), x = files_cat, value = TRUE, useBytes = TRUE, ignore.case = TRUE)
   # by geom
-  files_geom <- grep(pattern = geom, x = files_poly, value = TRUE, useBytes = TRUE)
+  files_geom <- grep(pattern = geom, x = files_poly, value = TRUE, useBytes = TRUE, ignore.case = TRUE)
   # by date
   if(!is.null(date)) {
-    file_selected <- grep(pattern = date, x = files_geom, value = TRUE, useBytes = TRUE)
+    file_selected <- grep(pattern = date, x = files_geom, value = TRUE, useBytes = TRUE, ignore.case = TRUE)
   } else if(isTRUE(most_recent)) { # get most recent file by sorting in decreasing order
       files_geom_sorted <- sort(files_geom, decreasing = TRUE)
       # get first file
@@ -84,6 +84,6 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
     stop("No related file found. Please use other argument values.", call. = FALSE)
   }
   # fix multibyte path bug #12
-  Encoding(file_selected) <- "UTF-8"
+  Encoding(file_selected) <- "latin1"
   sf::read_sf(file_selected)
 }
