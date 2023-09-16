f728b9e791fef002e9bbc016319e43ee0f8cdc96
Philipp Baumann <baumann-philipp@proton.me>
2023-09-16 15:07:15 +0200

    use default for macos unzip cli

--- R/bfs_get_base_maps.R
+++ R/bfs_get_base_maps.R
@@ -35,6 +35,7 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
   # get base map files if not present in cache folder
   dir <- tools::R_user_dir(package = "BFS")
   path_base_map <- paste0(dir, "/base_map_", asset_number)
+  browser()
 
   if (!dir.exists(path_base_map)) {
     dir.create(path_base_map, recursive = TRUE, showWarnings = FALSE)
@@ -53,7 +54,7 @@ bfs_get_base_maps <- function(geom = NULL, category = "gf", type = "Poly", date 
     }
 
     if (os == "osx") {
-      system2("unzip", "-v", paste0(path_base_map, ".zip"), "-d", path_base_map)
+      system2("unzip", paste0(path_base_map, ".zip"))
     } else {
       zip::unzip(
         zipfile = paste0(path_base_map, ".zip"),
