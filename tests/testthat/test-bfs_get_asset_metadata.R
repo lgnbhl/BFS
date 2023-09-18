a7785ad59355ea3b6c46910e2a5a9ba69cf96790
lgnbhl <felix.luginbuhl@protonmail.ch>
2023-09-18 12:11:16 +0200

    remove unit tests related to PXWeb API

--- tests/testthat/test-bfs_get_asset_metadata.R
+++
@@ -1,3 +0,0 @@
-test_that("bfs_get_asset_metadata() works", {
-  expect_identical(class(bfs_get_asset_metadata(number_asset = 24367729)), "list")
-})
--- tests/testthat/test-bfs_get_data.R
+++
@@ -1,3 +0,0 @@
-test_that("bfs_get_data() works", {
-  expect_true(any(class(bfs_get_data(number_bfs = "px-x-1502040100_131")) == "data.frame"))
-})
