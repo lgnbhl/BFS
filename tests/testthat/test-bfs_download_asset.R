53f6eac2c68c3deb868f858a8c20390edd253dac
lgnbhl <felix.luginbuhl@protonmail.ch>
2023-09-20 09:20:44 +0200

    fix test if file exists

--- tests/testthat/test-bfs_download_asset.R
+++ tests/testthat/test-bfs_download_asset.R
@@ -1,5 +1,5 @@
 test_that("bfs_download_asset() works", {
   tmp <- tempfile()
   asset_path <- bfs_download_asset(number_asset = "24025646", destfile = tmp)
-  expect_identical(tmp, asset_path)
+  expect_true(file.exists(asset_path))
 })
